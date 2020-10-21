70b20d4500576c2a07f6fa11c3991dd7ae21436b
KirillDan <kirilldan94@mail.ru>
2020-10-21 15:16:31 +0300

    4. candicates.jsp - список кандидатов. [#364311]
    	новый файл:    src/main/java/ru/job4j/dream/model/Candidate.java
    	изменено:      src/main/java/ru/job4j/dream/store/Store.java
    	новый файл:    src/main/webapp/candidates.jsp
    	изменено:      src/main/webapp/index.jsp
    	изменено:      src/main/webapp/posts.jsp

---
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -0,0 +1,84 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+public class Candidate {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String position;
+	public Candidate(int id, String firstname, String lastname, String position) {
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.position = position;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((position == null) ? 0 : position.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Candidate other = (Candidate) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Candidate [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", position=" + position
+				+ "]";
+	}
+   
+}
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.sql.Date;
@@ -13,19 +14,28 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "Development of algorithms for processing entities", new Date(120, 7, 5).toString()));
         posts.put(2, new Post(2, "Middle Java Job", "Web development", new Date(120, 6, 27).toString()));
         posts.put(3, new Post(3, "Senior Java Job", "Development of a distributed server part of the program", new Date(120, 7, 15).toString()));
         posts.put(3, new Post(3, "Senior/Tech Lead Java Job", "Development team management", new Date(120, 5, 15).toString()));
         posts.put(3, new Post(3, "Tech Lead/Architect Java Job", "Application architecture creation", new Date(120, 8, 7).toString()));
+        candidates.put(1, new Candidate(1, "Vasya", "Pupkin", "Junior Java"));
+        candidates.put(2, new Candidate(2, "Victor", "Pupkin", "Middle Java"));
+        candidates.put(3, new Candidate(3, "Andy", "Pupkin", "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
